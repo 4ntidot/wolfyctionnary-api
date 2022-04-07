80c3a3e58328cab3b79a80b7445db4d94d6dddfa
happex <98713994+nao-l@users.noreply.github.com>
2022-04-07 19:17:05 +0200

    ⚡️ Added letter indexing
    * Added letter indexing (eabd87f)
    * Fixed code typing error

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 		app.get("/", (req, res)) {
 			res.send("Welcome to the Naoctionnary api.");
 		}
-		fetch("./api/words.json") {
+		fetch("./api/words.json")
 			.then(response => response.json())
 			.then(data => {
 				app.get("/words", (req, res)) {
