eabd87f2a601e3b0dc0b9b865f036c8cc9f0b22e
happex <98713994+nao-l@users.noreply.github.com>
2022-04-07 19:14:43 +0200

    ⚡️ Added letter indexing
    * Added letter indexing
    * Patched word list sender

--- app.js
+++ app.js
@@ -7,15 +7,26 @@ const app = express();
 		app.get("/", (req, res)) {
 			res.send("Welcome to the Naoctionnary api.");
 		}
-		fetch("./api/words.json")
+		fetch("./api/words.json") {
 			.then(response => response.json())
 			.then(data => {
-				var words = data;
 				app.get("/words", (req, res)) {
 					res.send(words);
 				}
 				app.get("/words/:word", (req, res)) {
-					res.send(data.(req.user.name));
+					var word = data.indexOf(req.word.name);
+					res.send(data[word]);
+				}
+				app.get("/words/letter/:letter", (req, res)) {
+					var foundWords = [];
+					var i = 0;
+					for (i==0, i < data.length, i++) {
+						if (data[i].name.charAt(0) == req.word.name) {
+							foundWords.push(data[i]);
+						}
+						i += i;
+					}
+					res.send(foundWords);
 				}
 			})
 	}
